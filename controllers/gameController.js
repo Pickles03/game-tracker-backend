a82d667792be4c27af1127745854c15e1e53e13b
Pickles03 <elizaveta.kris@gmail.com>
2025-07-25 18:24:39 +0200

    added more games being able to load in + scrolling

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -2,26 +2,42 @@ const axios = require('axios');
 
 exports.getGames = async (req, res) => {
   try {
-    const genre = req.query.genre;
-    const response = await axios.get('https://api.rawg.io/api/games', {
-      params: {
-        key: process.env.RAWG_API_KEY,
-        search: req.query.search,
-        genres: genre,
-        page_size: 20
-      }
+    const {page = 1, genre = '', ordering = '', search = ''} = req.query;
+
+    const params = {
+      key: process.env.RAWG_API_KEY,
+      page,
+      page_size: 40,
+    }
+
+    if (genre) params.genres = genre;
+    if (ordering) params.ordering = ordering;
+    if (search) params.search = search;
+
+    console.log(`RAWG Request Params: ${JSON.stringify(params)}`);
+
+    const response = await axios.get('https://api.rawg.io/api/games', {params})
+
+    const results = response.data?.results || [];
+
+    const filtered = results.filter(game => {
+      return (
+        game.released &&
+        !isNaN(Date.parse(game.released)) &&
+        new Date(game.released) <= new Date()
+      );
     });
 
-    const formatGames = response.data.results.map(game => ({
+    const formatGames = filtered.map(game => ({
         id: game.id,
         name: game.name,
         slug: game.slug,
         released: game.released,
         background_image: game.background_image,
         rating: game.rating,
-        platforms: game.platforms.map(p => p.platform.name),
-        genres: game.genres.map(g => g.name),
-    }))
+        platforms: game.platforms.map(p => p.platform.name) || [],
+        genres: game.genres.map(g => g.name) || [],
+    }));
 
     res.status(200).json({results: formatGames});
   } catch (error) {
